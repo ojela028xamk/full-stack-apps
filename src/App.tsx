864862527ad78ea1ace12cbb2403f4e003178102
Jere Länsipii <jerelans91@gmail.com>
2022-08-25 19:07:25 +0300

    bootstrap update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import "./App.css";
+import "./App.scss";
+import { Col, Row, Container, Button } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <h1>Full Stack Apps</h1>
+      <Container>
+        <Row>
+          <Col className="column">
+            <h2>To-Do List</h2>
+            <Button variant="primary">Open</Button>
+          </Col>
+          <Col className="column">
+            <h2>Project 2</h2>
+            <Button variant="primary" disabled>
+              Open
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
